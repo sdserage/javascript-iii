1d444d430400cbd28db905f8024ecda7ca6e2e00
Scott Serage <xttocsurge51@gmail.com>
2017-09-06 14:05:39 -0600

    forInLoops_Delete.js: Completed showsValues

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -7,23 +7,23 @@
 
 // In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+}
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for(var key in values) {
+  console.log(values[key])
+}
 
 // In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+  console.log(key)
+}
 
 
 // ========================
@@ -33,6 +33,15 @@
 
 function showValues( obj ) {
   // CODE HERE
+  var str ='';
+  for(let key in obj){
+    if(str.length ===0){
+      str = obj[key];
+    } else {
+      str += obj[key];
+    }
+  }
+  return str;
 }
 
 
