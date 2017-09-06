e3796c2ba19936c5e51482ef8f0ba3f8f8ec1998
Scott Serage <xttocsurge51@gmail.com>
2017-09-06 12:21:49 -0600

    classes.js: Completed Employee Class

--- classes.js
+++ classes.js
@@ -22,8 +22,18 @@ Each employee can:
 call your class Employee and receive all the data in the constructor in the order listed
 */
 
-
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    // this = {}
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return this.first_name + " " + this.last_name + " Widget";
+  }
+}
 /*
 
 Next, make a manager for Widget Co.
