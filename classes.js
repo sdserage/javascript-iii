f8d2372f36ced0fbe6ed1d63c0d58e503ee3aa62
Scott Serage <xttocsurge51@gmail.com>
2017-09-06 13:04:46 -0600

    classes.js: Completed ProgressiveManager Class

--- classes.js
+++ classes.js
@@ -92,7 +92,44 @@ Everytime they fire an employee they get a bonus of $100 add to their .
 call you class ProgressiveManager
 */
 
-
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  makeWidget() {
+    return this.first_name + " " + this.last_name + " Widget";
+  }
+  hire(employee){
+    this.reports.push(employee);
+    this.changeTitle();
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+    this.changeTitle();
+  }
+  changeTitle(){
+    if(this.reports.length >= 101) {
+      this.title = 'Bestest Manager';
+    } else if(this.reports.length >= 51) {
+      this.title = 'Manager Plus';
+    } else if(this.reports.length >= 11) {
+      this.title = 'Manager';
+    } else if(this.reports.length >= 4) {
+      this.title = 'Mostly Manager';
+    } else if(this.reports.length >= 1) {
+      this.title = 'Barely Manager';
+    } else {
+      this.title = 'Not a manager';
+    }
+  }
+}
 
 
 /*
